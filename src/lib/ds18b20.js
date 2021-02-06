05a23cccb0153ce19c09e0990ac0604278538429
Andrew Trubchanin <andrew.trubchanin@gmail.com>
2021-02-06 12:28:58 +0200

    chore: test ds18b20

---
+++ src/lib/ds18b20.js
@@ -0,0 +1,16 @@
+import fs from 'fs/promises';
+
+export class Ds18b20 {
+  constructor(id) {
+  }
+
+  async read() {
+    return fs.readFile(`/sys/bus/w1/devices/${this.id}/temperature`);
+  }
+}
+
+const sensor = new Ds18b20('28-3c01d607e06a');
+
+const temp = await sensor.read();
+
+console.log('tmp', temp);
