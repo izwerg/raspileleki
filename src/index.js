8dbf1cde58466a19d16656a2df8e4db787a2cc3b
Andrew Trubchanin <andrew.trubchanin@gmail.com>
2021-02-07 19:34:13 +0200

    fix: managing relays

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { logger } from './lib/logger.js'
 import { config } from './config.js';
 
 import { Ds18b20 } from './lib/ds18b20.js';
+
+import { openBus } from './i2c.js';
 import { Relay } from './lib/relay.js';
 
 process
@@ -12,14 +14,30 @@ process
   .on('SIGINT', () => destroy())
   .on('uncaughtException', (err) => (logger.error(err), destroy(1)));
 
-logger.info('Process started.')
+function destroy(status = 0) {
+  destroyed$.next();
+  destroyed$.complete();
+  setTimeout(_ => (logger.info('Process stopped.'), process.exit(status)), 100);
+}
+  
+async function main() {
+  logger.info('Process started.')
 
-const destroyed$ = new Subject();
+  const destroyed$ = new Subject();
 
-const sensor = new Ds18b20(config.ds18b20Id);
-const relay = new Relay(config.relays[0]);
+  const bus = await openBus();
 
-// sensor.temperature$
+  const sensor = new Ds18b20(config.ds18b20Id);
+  const relay = new Relay(bus, config.relays[0]);
+
+  combineLatest([sensor.temperature$, relay.state$])
+  .pipe(takeUntil(destroyed$))
+  .subscribe(arr => {
+    const [temp, state] = arr;
+    logger.info(`t=${temp}, s=${state}`);
+  })
+
+  // sensor.temperature$
 //   .pipe(takeUntil(destroyed$))
 //   .subscribe(temp => {
 //     logger.info(`TEMP ${temp}`);
@@ -31,15 +49,4 @@ const relay = new Relay(config.relays[0]);
 //     logger.info(`RELAY ${state}`);
 //   });
 
-combineLatest([sensor.temperature$, relay.state$])
-  .pipe(takeUntil(destroyed$))
-  .subscribe(arr => {
-    const [temp, state] = arr;
-    logger.info(`t=${temp}, s=${state}`);
-  })
-
-function destroy(status = 0) {
-  destroyed$.next();
-  destroyed$.complete();
-  setTimeout(_ => (logger.info('Process stopped.'), process.exit(status)), 100);
 }
