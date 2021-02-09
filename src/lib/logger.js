9d41243e5d94379571705886b7286888a65cd72a
Andrew Trubchanin <andrew.trubchanin@gmail.com>
2021-02-09 21:07:38 +0200

    fix: logs timezone

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -5,6 +5,6 @@ export const logger = pino({
   level: config.debug ? 'debug' : 'info',
   redact: { paths: ['hostname', 'pid'], remove: true },
   prettyPrint: {
-    translateTime: 'yyyy-mm-dd\'T\'HH:MM:ss\'Z\'',
+    translateTime: 'SYS:yyyy-mm-dd\'T\'HH:MM:ss',
   },
 });
